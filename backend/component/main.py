10f9f76a052616fac559ce88621a34991e8139e1
KentTDang <kent_2015@outlook.com>
2024-07-14 20:20:57 -0400

    ➡️ Update to Vite Frontend

--- backend/component/main.py
+++ backend/component/main.py
@@ -1,2 +1,19 @@
 from flask import Flask, jsonify
 
+app = Flask(__name__)   #Create app instance
+
+@app.route("/api/users", methods=['GET'])
+def users():
+    return jsonify(
+        { 
+            "users": [
+                'kent',
+                'rachel',
+                'test',
+                'test'
+            ]
+        }
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
