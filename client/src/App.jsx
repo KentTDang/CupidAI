f05e922f5d44435752869d2a8e08aa02e771a869
KentTDang <kent_2015@outlook.com>
2024-07-16 10:47:19 -0400

    🏗️ Update Loading State

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Sidebar from './components/Sidebar/Sidebar';
-import Main from './components/Main/Main';
+import Sidebar from './components/Sidebar/Sidebar.jsx';
+import Main from './components/Main/Main.jsx';
 
 function App() {
 
--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,23 +1,26 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios';
-import './Main.css'
-import { assets } from '../../assets/assets'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Main.css";
+import { assets } from "../../assets/assets";
 
 export default function Main() {
-
-    const [task, setTask] = useState('');
+  const [task, setTask] = useState("");
   const [results, setResults] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [history, setHistory] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetchAPI();
-  }, [])
+  }, []);
 
   const fetchAPI = async () => {
     try {
-      const response = await axios.get('http://localhost:8080/api/disaster-response');
+      const response = await axios.get(
+        "http://localhost:8080/api/disaster-response"
+      );
       setTask(response.data);
-    }catch (e) {
+    } catch (e) {
       console.error(e);
     }
   };
@@ -31,72 +34,99 @@ export default function Main() {
     setResults([]);
     setLoading(true);
     try {
-      const response = await axios.post('http://localhost:8080/api/disaster-response', { task });
+      const response = await axios.post(
+        "http://localhost:8080/api/disaster-response",
+        { task }
+      );
       setResults(response.data);
+      setShowResult(true);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     } finally {
       setLoading(false);
-      setTask('');
     }
   };
 
   return (
     <div className="main">
-        <div className="nav">
-            <p>Tavily</p>
-            <img src="" alt="" />
-        </div>
-        <div className="main-container">
-            <div className="greet">
-                <p><span>Hello, Person</span></p>
-                <p>How can I help you today?</p>
-            </div>
-            <div className="cards">
-                <div className="card">
-                    <p>Suggest beautiful places to see on an upcoming road trip</p>
-                    <img src={assets.compass_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Suggest beautiful places to see on an upcoming road trip</p>
-                    <img src={assets.bulb_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Suggest beautiful places to see on an upcoming road trip</p>
-                    <img src={assets.message_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Suggest beautiful places to see on an upcoming road trip</p>
-                    <img src={assets.code_icon} alt="" />
-                </div>
-            </div>
-            <div className="results">
-        {results.map((item, index) => (
-          <>
-          <div key={index}>
-            <pre>{JSON.stringify(item, null, 2)}</pre>
-          </div>
-          </>
-          
-        ))}
+      <div className="nav">
+        <p>Tavily</p>
+        <img src="" alt="" />
       </div>
-            <div className="main-bottom">
-                {/* <div className="search-box"> */}
-                    <form onSubmit={handleSubmit} className='search-box'>
-                        <input 
-                        type="text"
-                        value={task}
-                        onChange={handleInputChange}
-                        placeholder='Enter a prompt here'/>
-                        <div>
-                        <button type='submit'><img src={assets.send_icon} alt="" /></button>
-                        </div>
-                    </form>
-                    
-                {/* </div> */}
-                <p className='bottom-info'>This AI can make mistakes. Check important info.</p>
-            </div>
+      <div className="main-container">
+      {!showResult? 
+        <div>
+
+        <div className="greet">
+          <p>
+            <span>Welcome</span>
+          </p>
+          <p>How can I help you today?</p>
         </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+          </div>
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+          </div>
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+          </div>
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+          </div>
+        </div>
+        </div>
+        :
+        null}
+        <div className="results">
+          {results.map((item, index) => (
+            <>
+              <div key={index}>
+                <pre>{JSON.stringify(item, null, 2)}</pre>
+              </div>
+            </>
+          ))}
+        </div>
+        <div className="main-bottom">
+          {!loading? 
+          <form onSubmit={handleSubmit} className="search-box">
+            <input
+              type="text"
+              value={task}
+              onChange={handleInputChange}
+              placeholder="Enter a prompt here"
+            />
+            <div>
+              <button type="submit" className="submit-btn">
+                <img src={assets.send_icon} alt="" />
+              </button>
+            </div>
+          </form> 
+          : 
+          <form onSubmit={handleSubmit} className="search-box">
+            <input
+              type="text"
+              value={task}
+              onChange={handleInputChange}
+              placeholder="Enter a prompt here"
+              disabled
+            />
+            <div>
+              <button type="submit" disabled className="submit-btn">
+                <img src={assets.send_icon} alt="" />
+              </button>
+            </div>
+          </form> 
+          }
+
+          
+          <p className="bottom-info">
+            This AI can make mistakes. Check important info.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
