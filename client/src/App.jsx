f075a21814353954b008f026fa94aaf0bedd26b2
KentTDang <kent_2015@outlook.com>
2024-07-16 09:53:31 -0400

    🎉

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,72 +1,14 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import Sidebar from './components/Sidebar/Sidebar';
 import Main from './components/Main/Main';
 
 function App() {
-  const [task, setTask] = useState('');
-  const [results, setResults] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    fetchAPI();
-  }, [])
-
-  const fetchAPI = async () => {
-    try {
-      const response = await axios.get('http://localhost:8080/api/disaster-response');
-      setTask(response.data);
-    }catch (e) {
-      console.error(e);
-    }
-  };
-
-  const handleInputChange = (e) => {
-    setTask(e.target.value);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setResults([]);
-    setLoading(true);
-    try {
-      const response = await axios.post('http://localhost:8080/api/disaster-response', { task });
-      setResults(response.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    } finally {
-      setLoading(false);
-      setTask('');
-    }
-  };
 
   return (
-
     <>
       <Sidebar />
       <Main />
     </>
-    // <div className="App">
-    //   <form onSubmit={handleSubmit}>
-    //     <input
-    //       type="text"
-    //       value={task}
-    //       onChange={handleInputChange}
-    //       placeholder="Enter task"
-    //     />
-    //     <button type="submit">Submit</button>
-    //   </form>
-    //   <div className="results">
-    //     {results.map((item, index) => (
-    //       <>
-    //       <div key={index}>
-    //         <pre>{JSON.stringify(item, null, 2)}</pre>
-    //       </div>
-    //       </>
-          
-    //     ))}
-    //   </div>
-    // </div>
   );
 }
 
--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
 import './Main.css'
 import { assets } from '../../assets/assets'
 
 export default function Main() {
+
+    const [task, setTask] = useState('');
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchAPI();
+  }, [])
+
+  const fetchAPI = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/api/disaster-response');
+      setTask(response.data);
+    }catch (e) {
+      console.error(e);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setTask(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setResults([]);
+    setLoading(true);
+    try {
+      const response = await axios.post('http://localhost:8080/api/disaster-response', { task });
+      setResults(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setLoading(false);
+      setTask('');
+    }
+  };
+
   return (
     <div className="main">
         <div className="nav">
@@ -32,13 +70,30 @@ export default function Main() {
                     <img src={assets.code_icon} alt="" />
                 </div>
             </div>
+            <div className="results">
+        {results.map((item, index) => (
+          <>
+          <div key={index}>
+            <pre>{JSON.stringify(item, null, 2)}</pre>
+          </div>
+          </>
+          
+        ))}
+      </div>
             <div className="main-bottom">
-                <div className="search-box">
-                    <input type="text" placeholder='Enter a prompt here'/>
-                    <div>
-                        <img src={assets.send_icon} alt="" />
-                    </div>
-                </div>
+                {/* <div className="search-box"> */}
+                    <form onSubmit={handleSubmit} className='search-box'>
+                        <input 
+                        type="text"
+                        value={task}
+                        onChange={handleInputChange}
+                        placeholder='Enter a prompt here'/>
+                        <div>
+                        <button type='submit'><img src={assets.send_icon} alt="" /></button>
+                        </div>
+                    </form>
+                    
+                {/* </div> */}
                 <p className='bottom-info'>This AI can make mistakes. Check important info.</p>
             </div>
         </div>
